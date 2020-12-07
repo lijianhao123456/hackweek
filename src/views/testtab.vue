15b97c9f9423412ea4ee68b1039f967b8e02bd2c
lijianhao <1075435016@qq.com>
2020-12-07 17:41:13 +0800

    获取到了tab的索引值，增加了下拉的功能，下一步下拉触发函数刷新数据

--- src/views/testtab.vue
+++ src/views/testtab.vue
@@ -1,20 +1,64 @@
 <template>
-  <van-tabs v-model="active" background="#ffcad4" swipeable>
-    <van-tab
-      v-for="index in 4"
-      :title="'选项 ' + index"
-      :key="index"
-      class="container"
-    >
-      <van-pull-refresh style="width=100%" v-model="isLoading" @refresh="onRefresh">
-
-      <div v-for="value in info" class="card">
-        <div>{{ value.owner }}</div>
-        <div>{{ value.info }}</div>
-        <van-icon class="like" name="like-o" />
-        <div>{{ value.likes }}</div>
-      </div>
-            </van-pull-refresh>
+  <van-tabs @click="onClick" background="#ffcad4" swipeable>
+    <van-tab :title="'时装 '">
+      <van-pull-refresh v-model="isLoading" @refresh="onRefresh">
+        <div class="container">
+          <div v-for="value in info" class="card">
+            <div>{{ value.owner }}</div>
+            <div>{{ value.info }}</div>
+            <van-icon class="like" name="like-o" />
+            <div>{{ value.likes }}</div>
+          </div>
+        </div>
+      </van-pull-refresh>
+    </van-tab>
+    <van-tab :title="'正装 '">
+      <van-pull-refresh v-model="isLoading" @refresh="onRefresh">
+        <div class="container">
+          <div v-for="value in info" class="card">
+            <div>{{ value.owner }}</div>
+            <div>{{ value.info }}</div>
+            <van-icon class="like" name="like-o" />
+            <div>{{ value.likes }}</div>
+          </div>
+        </div>
+      </van-pull-refresh>
+    </van-tab>
+    <van-tab :title="'理发 '">
+      <van-pull-refresh v-model="isLoading" @refresh="onRefresh">
+        <div class="container">
+          <div v-for="value in info" class="card">
+            <div>{{ value.owner }}</div>
+            <div>{{ value.info }}</div>
+            <van-icon class="like" name="like-o" />
+            <div>{{ value.likes }}</div>
+          </div>
+        </div>
+      </van-pull-refresh>
+    </van-tab>
+    <van-tab :title="'美甲 '">
+      <van-pull-refresh v-model="isLoading" @refresh="onRefresh">
+        <div class="container">
+          <div v-for="value in info" class="card">
+            <div>{{ value.owner }}</div>
+            <div>{{ value.info }}</div>
+            <van-icon class="like" name="like-o" />
+            <div>{{ value.likes }}</div>
+          </div>
+        </div>
+      </van-pull-refresh>
+    </van-tab>
+    <van-tab :title="'修眉 '">
+      <van-pull-refresh v-model="isLoading" @refresh="onRefresh">
+        <div class="container">
+          <div v-for="value in info" class="card">
+            <div>{{ value.owner }}</div>
+            <div>{{ value.info }}</div>
+            <van-icon class="like" name="like-o" />
+            <div>{{ value.likes }}</div>
+          </div>
+        </div>
+      </van-pull-refresh>
     </van-tab>
   </van-tabs>
 </template>
@@ -23,10 +67,12 @@ import Vue from "vue";
 import { Tab, Tabs } from "vant";
 import { Icon } from "vant";
 import { PullRefresh } from "vant";
+import { Toast } from "vant";
 
 Vue.use(PullRefresh);
 
 Vue.use(Icon);
+Vue.use(Toast);
 
 Vue.use(Tab);
 Vue.use(Tabs);
@@ -46,10 +92,13 @@ export default {
   methods: {
     onRefresh() {
       setTimeout(() => {
-        Toast('刷新成功');
+        Toast("刷新成功");
         this.isLoading = false;
         this.count++;
       }, 1000);
+    },
+    onClick(name, title) {
+      console.log(name);
     },
   },
 };
