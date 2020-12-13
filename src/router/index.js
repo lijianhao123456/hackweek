1cada891ac3197db50cdddf2f94e7f2db0abd49b
lijianhao <1075435016@qq.com>
2020-12-13 12:08:25 +0800

    修复了无法进入注册页面的bug

--- src/router/index.js
+++ src/router/index.js
@@ -99,7 +99,7 @@ const router = new VueRouter({
 });
 
 router.beforeEach((to, from, next) => {
-  if (to.path === '/' || to.path === '/login') return next();
+  if (to.path === '/' || to.path === '/login' || to.path ==='/register') return next();
   const tokenStr = window.sessionStorage.getItem('token')
   if (!tokenStr) return next("/login")
   next()
